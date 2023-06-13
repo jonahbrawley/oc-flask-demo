8569106967526b9ac4fff280ad75fa7e05e04339
jonahbrawley <jbrawley@redhat.com>
2023-06-13 12:24:40 -0700

    venv changes, add CORS module to flask

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import time
 from flask import Flask, render_template
+from flask_cors import CORS # CORS plugin
 
+# enable CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def animate_smiley():
@@ -10,5 +13,5 @@ def animate_smiley():
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True, host='0.0.0.0', port=port)
+	app.run(debug=False, host='0.0.0.0', port=port)
 
